1b9136875fa65c8fd1707fa638aaae122e9db9c2
Pratyush-linux-quiter <pratyushpal245@gmail.com>
2024-11-05 11:01:41 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 144533;
+let accountEmail="pratyush@google.com";
+var accountPassword = "12345"
+accountCity = "jaipur";
+let accountdate;
+
+// accountId=2;// not allowed
+accountEmail="kuni@google.com"
+accountPassword="7648"
+accountCity="dhenkanal"
+console.log(accountId);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountEmail,accountPassword,accountCity,accountdate]);
