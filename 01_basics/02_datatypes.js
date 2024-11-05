8715ac24e14dd5ed03df08550136ce45818dc551
Pratyush-linux-quiter <pratyushpal245@gmail.com>
2024-11-05 11:02:10 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello") // we are using node js not browser
+
+let name="pratyush";
+let age=20;
+let enabled=false;
+
+
+//primitive datatype
+// number => 2 to power 53
+//bigint 
+//string =>""
+// boolean =>true/false
+//null => standalone value
+//undefined =>
+// symbol=> unique    
+
+
+console.log(typeof undefined );// undefined
+console.log(typeof null); // object
