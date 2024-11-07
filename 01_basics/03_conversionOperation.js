95ee5d6ff8a235b5ae59e8bf84b789e3f6ef496f
Pratyush-linux-quiter <pratyushpal245@gmail.com>
2024-11-07 15:47:49 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let variable = Number(score);
+console.log(typeof variable);
+//score => output
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false =>0
+
+let isloggedin = 1;
+
+let booleanIsLoggedIn = Boolean(isloggedin);
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+// "hitesh" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
